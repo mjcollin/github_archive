941346ea15e064eab23bdce3b9e0efbffd277e59
Matthew Collins <matt@badmatt.com>
2019-11-25 21:30:06 -0500

    Add better looping and output

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,5 +3,13 @@ year=$1
 month=$2
 
 cd data
-for i in `seq 0 23` ; do wget -nc -q http://data.githubarchive.org/${year}-${month}-{01..31}-${i}.json.gz & done;
-wait
+for d in `seq -f "%02g" 1 31`
+do
+    for h in `seq -f "%02g" 0 23`
+    do
+        fn="${year}-${month}-${d}-${h}.json.gz"
+        echo "Starting download of $fn"
+        wget -nc -q http://data.githubarchive.org/$fn &
+    done
+    wait
+done
